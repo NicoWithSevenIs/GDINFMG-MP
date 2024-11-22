c0986e3bd748c54d1862a734c1e0a3f86ce1d748
Enrico Tolentino <80933336+NicoWithSevenIs@users.noreply.github.com>
2024-11-22 18:50:03 +0800

    Pokemon Battle Instance draft

--- Assets/Scripts/Data/Pokemon/Pokemon_Battle_Instance.cs
+++ Assets/Scripts/Data/Pokemon/Pokemon_Battle_Instance.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pokemon_Battle)Instace : MonoBehaviour
+public class Pokemon_Battle_Instance 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    private Pokemon pokemon;
+    private Stat totalStats;
+    private StatModHandler modHandler = new();
+
+    public Stat stat { 
+        get => modHandler.ApplyMods(totalStats);
     }
 
-    // Update is called once per frame
-    void Update()
+    private float currentHP;
+
+    public float CurrentHealth { get => currentHP; }
+
+    public Pokemon_Battle_Instance(Pokemon pokemon)
     {
-        
+        this.pokemon = pokemon;
+        totalStats = pokemon.GetTotalStat();
+        currentHP = stat.Health;
     }
+
+    public void TakeDamage(float damage)
+    {
+
+    }
+
+
 }
