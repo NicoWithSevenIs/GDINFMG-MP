79ba117b70e9ba583a1d79fc5e7ff2fd46ea7061
Enrico Tolentino <80933336+NicoWithSevenIs@users.noreply.github.com>
2024-11-24 11:52:06 +0800

    hotfix

--- Assets/Scripts/Data/Pokemon/Pokemon_Battle_Instance.cs
+++ Assets/Scripts/Data/Pokemon/Pokemon_Battle_Instance.cs
@@ -82,6 +82,54 @@ public class Pokemon_Battle_Instance
         });
 
     }
-    
+
+    public static float CalculateDamage(
+       Pokemon_Battle_Instance attacker,
+       Pokemon_Battle_Instance target,
+       float power,
+       EType type,
+       EMoveType moveClass,
+       out bool isACriticalStrike
+   )
+    {
+        if (moveClass == EMoveType.STATUS)
+        {
+            isACriticalStrike = false;
+            return 0f;
+        }
+
+
+        EStatType attackingStat = moveClass == EMoveType.PHYSICAL ? EStatType.ATTACK : EStatType.SPECIAL_ATTACK;
+        EStatType defendingStat = moveClass == EMoveType.PHYSICAL ? EStatType.DEFENSE : EStatType.SPECIAL_DEFENSE;
+
+        float A = attacker.stat.GetByEnum(attackingStat);
+        float D = target.stat.GetByEnum(defendingStat);
+
+        float levelMultiplier = 2 * (float)Pokemon_Battle_Instance.LEVEL / 5 + 2;
+        float baseDamage = levelMultiplier * power * (A / D) / 50 + 2;
+
+
+        //STAB
+        if (attacker.Pokemon.data.type1 == type || attacker.Pokemon.data.type2 == type)
+            baseDamage *= 1.5f;
+
+        //CRIT
+        isACriticalStrike = Random.Range(1, 25) == 24;
+        if (isACriticalStrike)
+            baseDamage *= (2 * Pokemon_Battle_Instance.LEVEL + 5) / Pokemon_Battle_Instance.LEVEL + 5;
+
+        //Type Effectiveness
+        baseDamage *= TypeChecker.GetEffectivenessMultiplier(type, target.Pokemon.data.type1, target.Pokemon.data.type2);
+
+        //Random
+        baseDamage *= Random.Range(85f, 100f) / 100f;
+
+        //Burn
+        if (attacker.AilmentType == EAilmentType.BURN)
+            baseDamage *= 0.5f;
+
+
+        return baseDamage;
+    }
 
 }
