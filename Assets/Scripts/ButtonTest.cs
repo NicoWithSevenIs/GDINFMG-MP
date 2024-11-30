15b928e0e35a97e780d333d09074fb91d9eba0a5
DerekColbeeUy <derek_colbee_uy@dlsu.edu.ph>
2024-11-30 19:32:25 +0800

    added buttons and colliders

---
+++ Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Heal()
+    {
+        Debug.Log("All pokemon healed");
+    }
+
+    public void PC()
+    {
+        Debug.Log("Open PC");
+    }
+}
