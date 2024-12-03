60aa9fb896e16b2b7209d789ea856c094a450401
uwu-owo-o3o <lance_d_ong@dlsu.edu.ph>
2024-12-03 09:03:16 +0800

    admin add progress

--- Assets/PHP/admin_add_pokemon.php
+++ Assets/PHP/admin_add_pokemon.php
@@ -31,7 +31,7 @@ $moveID5 = $_POST["moveID5"];
 $moveID6 = $_POST["moveID6"];
 
 // check if name exists //
-$namecheckquery = "SELECT pokemonID FROM players WHERE pokemonID = '$pokemonID'; ";
+$namecheckquery = "SELECT pokemonID FROM pokemon WHERE pokemonID = '$pokemonID'; ";
 
 $namecheck = mysqli_query($con, $namecheckquery) or die("2: Name Check Query Failed."); //error code 2 = namecheck query failed
 
@@ -51,7 +51,7 @@ if (mysqli_num_rows($namecheck2) > 0) {
     exit();
 }
 
-$move_pool_check_query == "SELECT moveID1 FROM move;";
+$move_pool_check_query = "SELECT moveID FROM move WHERE moveID = '$moveID1';";
 $move_pool_check = mysqli_query($con, $move_pool_check_query);
 if (mysqli_num_rows($move_pool_check) == 0) {
     echo "Move ID 1 Doesn't Exist in Move!";
@@ -59,49 +59,49 @@ if (mysqli_num_rows($move_pool_check) == 0) {
 }
 
 
-$move_pool_check_query2 == "SELECT moveID2 FROM move;";
+$move_pool_check_query2 = "SELECT moveID FROM move WHERE moveID = '$moveID2';";
 $move_pool_check2 = mysqli_query($con, $move_pool_check_query2);
 if (mysqli_num_rows($move_pool_check2) == 0) {
     echo "Move ID 2 Doesn't Exist in Move!";
     exit();
 }
 
-$move_pool_check_query3 == "SELECT moveID3 FROM move;";
-$move_pool_check2 = mysqli_query($con, $move_pool_check_query3);
+$move_pool_check_query3 = "SELECT moveID FROM move WHERE moveID = '$moveID3';";
+$move_pool_check3 = mysqli_query($con, $move_pool_check_query3);
 if (mysqli_num_rows($move_pool_check3) == 0) {
     echo "Move ID 3 Doesn't Exist in Move!";
     exit();
 }
 
-$move_pool_check_query4 == "SELECT moveID4 FROM move;";
-$move_pool_check2 = mysqli_query($con, $move_pool_check_query4);
+$move_pool_check_query4 = "SELECT moveID FROM move WHERE moveID = '$moveID4';";
+$move_pool_check4 = mysqli_query($con, $move_pool_check_query4);
 if (mysqli_num_rows($move_pool_check4) == 0) {
     echo "Move ID 4 Doesn't Exist in Move!";
     exit();
 }
 
-$move_pool_check_query5 == "SELECT moveID5 FROM move;";
+$move_pool_check_query5 = "SELECT moveID FROM move WHERE moveID = '$moveID5';";
 $move_pool_check5 = mysqli_query($con, $move_pool_check_query5);
 if (mysqli_num_rows($move_pool_check5) == 0) {
     echo "Move ID 5 Doesn't Exist in Move!";
     exit();
 }
 
-$move_pool_check_query6 == "SELECT moveID6 FROM move;";
+$move_pool_check_query6 = "SELECT moveID FROM move WHERE moveID = '$moveID6';";
 $move_pool_check6 = mysqli_query($con, $move_pool_check_query6);
 if (mysqli_num_rows($move_pool_check6) == 0) {
     echo "Move ID 6 Doesn't Exist in Move!";
     exit();
 }
 
-$send_data_query = "INSERT INTO pokemon (pokemonID, pokemonName, pokemonType1, pokemonType2, pokemonWeight, pokemonHeight, spriteID) VALUES ('$pokemonID', '$pokemonName', '$pokemonType1', '$pokemonType2', '$pokemonWeight', '$pokemonHeight', '$spriteID');";   
-mysqli_query($con, $send_data_query) or die("4: Insert user details query failed.");
+// $send_data_query = "INSERT INTO pokemon (pokemonID, pokemonName, pokemonType1, pokemonType2, pokemonWeight, pokemonHeight, spriteID) VALUES ('$pokemonID', '$pokemonName', '$pokemonType1', '$pokemonType2', '$pokemonWeight', '$pokemonHeight', '$spriteID');";   
+// mysqli_query($con, $send_data_query) or die("4: Insert user details query failed.");
 
-$send_data_query2 = "INSERT INTO pokestats (hp, attack, special_attack, defense, special_defense, speed) VALUES ('$hp', '$attack', '$special_attack', '$defense', '$special_defense', '$speed');";   
-mysqli_query($con, $send_data_query2) or die("4: Insert user details query failed.");
+// $send_data_query2 = "INSERT INTO pokestats (hp, attack, special_attack, defense, special_defense, speed) VALUES ('$hp', '$attack', '$special_attack', '$defense', '$special_defense', '$speed');";   
+// mysqli_query($con, $send_data_query2) or die("4: Insert user details query failed.");
 
-$send_data_query3 =  "INSERT INTO movepool (pokemonID, moveID1, moveID2, moveID3, moveID4, moveID5, moveID6) VALUES ('$pokemonID', '$moveID1', '$moveID2', '$moveID3', '$moveID4', '$moveID5', '$moveID6');";   
-mysqli_query($con, $send_data_query3) or die("4: Insert user details query failed.");
+// $send_data_query3 =  "INSERT INTO movepool (pokemonID, moveID1, moveID2, moveID3, moveID4, moveID5, moveID6) VALUES ('$pokemonID', '$moveID1', '$moveID2', '$moveID3', '$moveID4', '$moveID5', '$moveID6');";   
+// mysqli_query($con, $send_data_query3) or die("4: Insert user details query failed.");
 
 echo "Success!";
 
--- Assets/Scripts/Managers/Database/Admin_AddPokemon.cs
+++ Assets/Scripts/Managers/Database/Admin_AddPokemon.cs
@@ -1,19 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class Admin_AddPokemon : MonoBehaviour
 {
-  private void callAdminAdd()
+  public List<TMP_InputField> pokemonInputs = new List<TMP_InputField>();
+  public List<TMP_InputField> statInputs = new List<TMP_InputField>();
+  public List<TMP_InputField> movepoolInputs = new List<TMP_InputField>();
+
+    public void callAdminAdd()
   {
+        
         StartCoroutine(AdminAddPokemon());
   }
 
   private IEnumerator AdminAddPokemon()
   {
         WWWForm form = new WWWForm();
-        form.AddField("playerID", PlayerManager.playerID);
+        form.AddField("pokemonID", pokemonInputs[0].text);
+        form.AddField("pokemonName", pokemonInputs[1].text);
+        form.AddField("pokemonType1", pokemonInputs[2].text);
+        form.AddField("pokemonType2", pokemonInputs[3].text);
+        form.AddField("pokemonWeight", pokemonInputs[4].text);
+        form.AddField("pokemonHeight", pokemonInputs[5].text);
+        form.AddField("spriteID", pokemonInputs[6].text);
+
+        form.AddField("hp", statInputs[0].text);
+        form.AddField("attack", statInputs[1].text);
+        form.AddField("special_attack", statInputs[2].text);
+        form.AddField("defense", statInputs[3].text);
+        form.AddField("special_defense", statInputs[4].text);
+        form.AddField("speed", statInputs[5].text);
+
+        form.AddField("moveID1", movepoolInputs[0].text);
+        form.AddField("moveID2", movepoolInputs[1].text);
+        form.AddField("moveID3", movepoolInputs[2].text);
+        form.AddField("moveID4", movepoolInputs[3].text);
+        form.AddField("moveID5", movepoolInputs[4].text);
+        form.AddField("moveID6", movepoolInputs[5].text);
 
         UnityWebRequest retrieve_req = UnityWebRequest.Post("http://localhost/admin_add_pokemon.php", form);
         yield return retrieve_req.SendWebRequest();
@@ -28,11 +55,11 @@ public class Admin_AddPokemon : MonoBehaviour
             string[] retrieve_result = retrieve_req.downloadHandler.text.Split('\t');
             if (retrieve_result[0].Contains("Success"))
             {
-
+                Debug.Log(retrieve_result[0]);
             }
             else
             {
-
+                Debug.Log(retrieve_result[0]);
             }
         }
         else
