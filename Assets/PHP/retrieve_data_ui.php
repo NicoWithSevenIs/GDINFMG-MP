f465e80539572e7304b8880747169d7ef8f9f69b
uwu-owo-o3o <lance_d_ong@dlsu.edu.ph>
2024-12-07 19:32:19 +0800

    parses pokemon data just fine

--- Assets/PHP/retrieve_data_ui.php
+++ Assets/PHP/retrieve_data_ui.php
@@ -21,20 +21,42 @@ while ($pokemon_ui_info = mysqli_fetch_assoc($pokemon_ui)) {
     $pokemonType2 = $pokemon_ui_info["pokemonType2"];
     $spriteID = $pokemon_ui_info["spriteID"];
     
-    echo $pokemonID;
+    echo "PokemonID: " . $pokemonID;
     echo "\t";
-    echo $pokemonName;
+    echo "PokemonName: " . $pokemonName;
     echo "\t";
-    echo $pokemonType1;
+    echo "PokemonType1: " . $pokemonType1;
     echo "\t";
-    echo $pokemonType2;
+    echo "PokemonType2: " . $pokemonType2;
     echo "\t";
-    echo $spriteID;    
+    echo "SpriteID: " . $spriteID;    
+    echo "\t";
 }
 
-$pokemon_ui_query = "SELECT (*) FROM move;";
-$pokemon_ui = mysqli_query($con, $pokemon_ui_query) or die("2: RETRIEVAL OF ALL MONS FAILED.");
+$move_ui_query = "SELECT * FROM move;";
+$move_ui = mysqli_query($con, $move_ui_query) or die("2: RETRIEVAL OF ALL MONS FAILED.");
 
+while ($move_ui_info = mysqli_fetch_assoc($move_ui)) {
+    $moveID = $move_ui_info["moveID"];
+    $moveName = $move_ui_info["moveName"];
+    $moveDescription = $move_ui_info["moveDescription"];
+    $moveType = $move_ui_info["moveType"];
+    $moveGroup = $move_ui_info["moveGroup"];
+    $movePower = $move_ui_info["movePower"];
+
+    echo "MoveID: " . $moveID;
+    echo "\t";
+    echo "MoveName: " . $moveName;
+    echo "\t";
+    echo "MoveDescription: " . $moveDescription;
+    echo "\t";
+    echo "MoveType: " . $moveType;
+    echo "\t";
+    echo "MoveGroup: " . $moveGroup;    
+    echo "\t";
+    echo "MovePower: " . $movePower;
+    echo "\t";
+}
 
 
 ?>
--- Assets/Scripts/Managers/Database/Admin_Ui.cs
+++ Assets/Scripts/Managers/Database/Admin_Ui.cs
@@ -5,17 +5,17 @@ using UnityEngine.Networking;
 
 public class Admin_Ui : MonoBehaviour
 {
-    public List<int> spriteID_list;
-    public List<int> pokemonID_list;
-    public List<string> pokemonName_list;
-    public List<string> pokemonType1_list;
-    public List<string> pokemonType2_list;
+    public List<int> spriteID_list = new List<int>();
+    public List<int> pokemonID_list = new List<int>();
+    public List<string> pokemonName_list = new List<string>();
+    public List<string> pokemonType1_list = new List<string>();
+    public List<string> pokemonType2_list = new List<string>();
     
-    public List<MoveData> moveDatas;
+    public List<MoveData> moveDatas = new List<MoveData>();
 
     public void callRetrieveUI()
     {
-
+        StartCoroutine(RetrieveDataUI());
     }
 
     public IEnumerator RetrieveDataUI()
@@ -33,7 +33,11 @@ public class Admin_Ui : MonoBehaviour
             string[] retrieve_result = retrieve_req.downloadHandler.text.Split('\t');
             if (retrieve_result[0].Contains("Success"))
             {
-                Debug.Log(retrieve_result[0]);
+                foreach (string s in retrieve_result)
+                {
+                    //Debug.Log(s);
+                    this.DecipherPokemonData(s);
+                }
             }
             else
             {
@@ -46,7 +50,34 @@ public class Admin_Ui : MonoBehaviour
         }
     }
 
-    
+    private void DecipherPokemonData(string value)
+    {
+        if (value.Contains("PokemonID: "))
+        {
+            string pokemonID = value.Substring(11);
+            this.pokemonID_list.Add(int.Parse(pokemonID));
+        }
+        else if (value.Contains("PokemonName: "))
+        {
+            string pokemonName = value.Substring(13);
+            this.pokemonName_list.Add(pokemonName);
+        }
+        else if (value.Contains("PokemonType1: "))
+        {
+            string pokemonType1 = value.Substring(14);
+            this.pokemonType1_list.Add(pokemonType1);
+        }
+        else if (value.Contains("PokemonType2: "))
+        {
+            string pokemonType2 = value.Substring(14);
+            this.pokemonType2_list.Add(pokemonType2);  
+        }
+        else if (value.Contains("SpriteID: "))
+        {
+            string spriteid = value.Substring(10);
+            this.spriteID_list.Add(int.Parse(spriteid));
+        }
+    }
 }
 
 
